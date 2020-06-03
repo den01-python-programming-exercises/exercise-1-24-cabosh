197295ad9860dfc7c792e688b86628708f37a5fc
cabosh <callum.bosher@gmail.com>
2020-06-03 11:11:04 +0100

    Attempted commit

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
     #write your code below this line
+    age = int(input("How old are you? "))
+
+    if age < 18:
+        print("You are not an adult")
+    elif age >= 18:
+        print("You are an adult")
 
 if __name__ == '__main__':
     main()
